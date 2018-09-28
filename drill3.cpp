8401f3bea022c1aa2e4df84c0943552e3312fb67
Tibi1212 <43321960+Tibi1212@users.noreply.github.com>
2018-09-28 14:27:08 +0200

    Add files via upload

---
+++ drill3.cpp
@@ -0,0 +1,45 @@
+#include "std_lib_facilities.h"
+
+int main(){
+
+cout<<"Írd be az ismerősöd nevét akinek írni szeretnél:\n";
+string first_name;
+cin>>first_name;
+cout<<"Írd be egy másik ismerősöd nevét:\n";
+string friend_name;
+cin>>friend_name;
+cout<<"Írj 'f'-et, ha az ismerősöd férfi és 'n'-t, ha az ismerősöd nő.\n"
+char friend_sex;
+cin>>friend_sex;
+cout<<"Írd be a fogadó életkorát:\n";
+int age;
+cin>>age;
+
+if (age>0&&age<100)
+
+{
+cout<<"\nKedves "<<first_name<<",\n\Hogy vagy?Köszönöm jól.Hiányzol.\nTalálkozunk"<<friend_name<<"később?\n";
+
+if (friend_sex=='f');
+cout<<"Ha látod "<<friend_name<<" hívjon fel légyszíves.\n";
+if (friend_sex=='n')
+cout<<"Ha látod"<<friend_name<<"hívjon fel légyszíves.\n"; 
+cout<<"Azt hallottam, hogy éppen most volt a szülinapod és már"<<age<<" éves vagy.\n";
+if(age<14)
+cout<<"A következő évben"<<age+1<<.\n";
+if (age==17)
+cout<<"A következő évben már szavazhatsz.\n";
+if (age>70)
+cout<<"Remélem élvezed a nyugdíjat\n";
+cout<<"Üdvözlettel,\n\n\n";
+
+}
+
+else
+{
+cout<<"hiba\n";
+
+}
+}
+
+
