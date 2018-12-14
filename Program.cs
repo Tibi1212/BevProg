a062716833cef2d97028491ac1c3b9feec607d6c
Zoli1212 <43321960+Zoli1212@users.noreply.github.com>
2018-12-14 22:00:47 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,58 +2,49 @@
  * Created by SharpDevelop.
  * User: Lenovo
  * Date: 2018.12.14.
- * Time: 21:20
+ * Time: 20:45
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 using System.IO;
 
-namespace nyuszi
+
+namespace balazs1
 {
 	class Program
 	{
 		public static void Main(string[] args)
 		{
-			int tojas = 0;
-			int suti = 0;
-			int csoki = 0;
-			string sor = " ";
-			int i = 0;
+			int szam;
+			int sor = 0;
 			
-			string[] nevek = new string[100];
+			Console.WriteLine("Adj meg egy számot!: ");
 			
-			StreamReader sr = new StreamReader("nyuszi.txt");
+			szam = Convert.ToInt32(Console.ReadLine());
+			
+			sor++;
+			
+			StreamReader sr = new StreamReader("szavak.txt");
 			
 			while(!sr.EndOfStream)
 			{
-				sor = sr.ReadLine();
 				
-				nevek[i] = sor;
-				i++;
-				sor = sr.ReadLine();
-				tojas += Convert.ToInt32(sor);
-				sor = sr.ReadLine();
-				csoki += Convert.ToInt32(sor);
-				sor = sr.ReadLine();
-				suti += Convert.ToInt32(sor);
+				sr.ReadLine();
 				
+				if(sor%szam == 0);
+				{
+				Console.WriteLine(sr.ReadLine());
+				}
+				sor++;
+				
+			}
 			
-			}
-			Console.WriteLine("A meglocsolt hölgyek neve: ");
-			for(int j = 0;j<=i; j++)
-			{
-				Console.WriteLine(nevek[j]);
-				
-			}
-		
-			Console.WriteLine("tojások száma: {0}", tojas);
-			                  
-			Console.WriteLine("csokik száma: {0}", csoki);
-			                  
-			Console.WriteLine("sütik száma: {0}", suti);
+			Console.WriteLine(sr.ReadLine());
+			
 			
 			Console.Write("Press any key to continue . . . ");
+			
 			sr.Close();
 			Console.ReadKey(true);
 		}
