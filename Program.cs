f490517c30784a15348f95193cd6c093c569c6f2
Zoli1212 <43321960+Zoli1212@users.noreply.github.com>
2018-12-15 08:34:13 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,52 +1,52 @@
 ﻿/*
  * Created by SharpDevelop.
  * User: Lenovo
- * Date: 2018.12.14.
- * Time: 20:45
+ * Date: 2018.12.15.
+ * Time: 8:30
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using System.IO;
 
-
-namespace balazs1
+namespace kapcsolo
 {
+
 	class Program
 	{
 		public static void Main(string[] args)
 		{
-			int szam;
-			int sor = 0;
+			string szin = "";
 			
-			Console.WriteLine("Adj meg egy számot!: ");
+			Console.WriteLine("Adj meg egy színt!");
 			
-			szam = Convert.ToInt32(Console.ReadLine());
+			szin = Console.ReadLine();
 			
-			sor++;
-			
-			StreamReader sr = new StreamReader("szavak.txt");
-			
-			while(!sr.EndOfStream)
+			switch(szin)
 			{
-				
-				sr.ReadLine();
-				
-				if(sor%szam == 0);
-				{
-				Console.WriteLine(sr.ReadLine());
-				}
-				sor++;
-				
+				case "piros":
+					Console.WriteLine("Amegadott szín piros.");
+					
+					break;
+					case "kék":
+					Console.WriteLine("A megadott szín kék.");
+					
+						break;
+						
+					case "zöld":
+						Console.WriteLine("A megadott szín zöld");
+			
+					break;
+						
+				default:
+					Console.WriteLine("A megadott színt nem ismerem");
+						break;
+					
 			}
 			
-			Console.WriteLine(sr.ReadLine());
-			
-			
 			Console.Write("Press any key to continue . . . ");
-			
-			sr.Close();
 			Console.ReadKey(true);
 		}
 	}
 }
+
+	
