3806fc3753e0c4a4af6545cbafcce3b961e2b8d4
Zoli1212 <43321960+Zoli1212@users.noreply.github.com>
2018-12-26 16:06:07 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,57 +1,40 @@
 ﻿/*
  * Created by SharpDevelop.
  * User: Lenovo
- * Date: 2018.12.15.
- * Time: 8:30
+ * Date: 2018.12.26.
+ * Time: 11:13
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using System.IO;
 
-namespace kapcsolo
+namespace netacademia2
 {
-
 	class Program
 	{
 		public static void Main(string[] args)
 		{
-			string szin = "";
 			
-			Console.WriteLine("Adj meg egy színt!");
-			
-			szin = Console.ReadLine();
-			
-			switch(szin)
+			if(DateTime.Today.DayOfWeek==DayOfWeek.Tuesday)
+				
 			{
-				case "piros":
-					Console.WriteLine("Amegadott szín piros.");
-					
-					break;
-					case "kék":
-					Console.WriteLine("A megadott szín kék.");
-					
-						break;
-						
-					case "zöld":
-						Console.WriteLine("A megadott szín zöld");
-			
-					break;
-						
-				default:
-					Console.WriteLine("A megadott színt nem ismerem");
-						break;
-					
+				Console.WriteLine("Valamilyen halas recept hozzávalók");
 			}
+			else
+			{
+				
 			
-			StreamWriter sw = new StreamWriter("szinek.txt", true);
-			sw.WriteLine(szin);
-			sw.Close();
+			Console.WriteLine("Marhahús");
+			Console.WriteLine("Vöröshagyma");
+			Console.WriteLine("Olaj");
+			Console.WriteLine("Pirospaprika");
+			
+			Console.WriteLine("Hello World!");
+			}
+			// TODO: Implement Functionality Here
 			
 			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			Console.ReadLine();
 		}
 	}
 }
-
-	
