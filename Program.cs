77162cc385ce16a8e4e8bf3bc4c2f19a269a77a6
Zoli1212 <43321960+Zoli1212@users.noreply.github.com>
2019-03-23 20:30:24 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,54 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Lenovo
- * Date: 2018.12.26.
- * Time: 11:13
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
-namespace netacademia2
+namespace nyuszi
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
-			
-			if(DateTime.Today.DayOfWeek==DayOfWeek.Tuesday)
-				
-			{
-				Console.WriteLine("Valamilyen halas recept hozzávalók");
-			}
-			else
-			{
-				
-			
-			Console.WriteLine("Marhahús");
-			Console.WriteLine("Vöröshagyma");
-			Console.WriteLine("Olaj");
-			Console.WriteLine("Pirospaprika");
-			
-			Console.WriteLine("Hello World!");
-			}
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadLine();
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("nyuszi.txt");
+
+            int tojas = 0;
+            int csoki = 0;
+            int suti = 0;
+
+            string sor = "";
+
+            Console.WriteLine("A hölgyek neve: ");
+            Console.WriteLine();
+
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+                sor = sr.ReadLine();
+                tojas += Convert.ToInt32(sor);
+                sor = sr.ReadLine();
+                csoki += Convert.ToInt32(sor);
+                sor = sr.ReadLine();
+                suti += Convert.ToInt32(sor);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Tojások száma: {0}", tojas);
+            Console.WriteLine("Csokik száma: {0}", csoki);
+            Console.WriteLine("Sütik száma: {0}", suti);
+
+            sr.Close();
+
+            //filekiiratás
+            StreamWriter sw = new StreamWriter("osszeg.txt", true);
+            sw.WriteLine("Tojások száma: {0}", tojas);
+            sw.WriteLine("Sutik száma: {0}", suti);
+            sw.WriteLine("Csokik száma: {0}", csoki);
+
+            sw.Close();
+            Console.ReadKey(true);
+
+        
+        }
+    }
 }
